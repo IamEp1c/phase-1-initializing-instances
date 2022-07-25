4bf08efaeb8ea4bfc49badb15b7b434593e657d7
Waqas Anjum <Qanjum96@gmail.com>
2022-07-24 23:52:44 -0400

    practicing classes in javascript

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink){
+        this.food = food
+        this.drink = drink
+    }
+}
+
+let breakfast1 = new Breakfast("eggs", "Orange Juice")
