8c4d81235aaad36a437c25dd7990dcbb4f8ff4fb
Waqas Anjum <Qanjum96@gmail.com>
2022-07-25 00:19:39 -0400

    completed reviewing instances in javascript

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ class Breakfast {
 }
 
 let breakfast1 = new Breakfast("eggs", "Orange Juice")
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad 
+        this.soup = soup 
+        this.drink = drink
+    }
+}
+
+let lunch1 = new Lunch("greek salad", "onion soup", "coke")
+
+
+class Dinner {
+    #dessert; 
+    constructor(salad, soup, entree, dessert){
+        this.#dessert = dessert
+        this.salad = salad 
+        this.soup = soup 
+        this.entree = entree
+    }
+}
+
+let dinner1 = new Dinner("balsamic salad", "potato soup", "steak", "Cookies")
